8191f36b160bd5a0082cdc2e2be1dc40ec88c4d0
Marcin Kuthan <marcin.kuthan@allegro.pl>
2025-05-10 17:49:28 +0200

    Introduce abstraction for adopting different inverter types

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from unittest.mock import AsyncMock, patch
+from unittest.mock import patch
 
 from homeassistant.config_entries import ConfigEntryState
 from homeassistant.const import CONF_API_KEY, CONF_API_TOKEN, CONF_TOKEN, Platform
@@ -12,50 +12,53 @@ from custom_components.solis_cloud_control import async_migrate_entry
 from custom_components.solis_cloud_control.const import CONF_INVERTER_SN, DOMAIN
 
 
-async def test_async_setup_entry(hass: HomeAssistant):
-    any_inverter_sn = "any inverter sn"
+async def test_async_setup_entry(hass, mock_api_client, mock_config_entry, any_inverter):
+    mock_api_client.read_batch.return_value = dict.fromkeys(any_inverter.all_cids(), None)
 
-    config_entry = MockConfigEntry(
-        version=2,
-        domain=DOMAIN,
-        data={
-            CONF_API_KEY: "any api key",
-            CONF_API_TOKEN: "any api token",
-            CONF_INVERTER_SN: any_inverter_sn,
-        },
-    )
-    config_entry.add_to_hass(hass)
-
-    mock_api_client = AsyncMock()
-    with patch(
-        "custom_components.solis_cloud_control._create_api_client",
-        return_value=mock_api_client,
+    with (
+        patch(
+            "custom_components.solis_cloud_control._create_api_client",
+            return_value=mock_api_client,
+        ),
+        patch(
+            "custom_components.solis_cloud_control.create_inverter_info",
+            return_value=any_inverter.info,
+        ),
+        patch(
+            "custom_components.solis_cloud_control.create_inverter",
+            return_value=any_inverter,
+        ),
     ):
-        await hass.config_entries.async_setup(config_entry.entry_id)
+        await hass.config_entries.async_setup(mock_config_entry.entry_id)
         await hass.async_block_till_done()
 
-    assert config_entry.state is ConfigEntryState.LOADED
+    # check if the config entry is loaded
+    assert mock_config_entry.state is ConfigEntryState.LOADED
 
     # coordinator.async_config_entry_first_refresh()
     assert mock_api_client.read_batch.called
 
-    assert hasattr(config_entry, "runtime_data")
-    assert config_entry.runtime_data.coordinator is not None
+    assert hasattr(mock_config_entry, "runtime_data")
+    assert mock_config_entry.runtime_data.coordinator is not None
+    assert mock_config_entry.runtime_data.inverter is not None
 
     # check device registration
     device_registry = dr.async_get(hass)
-    device = device_registry.async_get_device(identifiers={(DOMAIN, any_inverter_sn)})
+    device = device_registry.async_get_device(identifiers={(DOMAIN, any_inverter.info.serial_number)})
 
     assert device is not None
     assert device.manufacturer == "Solis"
-    assert device.name == f"Inverter Control {any_inverter_sn}"
+    assert device.name == f"Inverter Control {any_inverter.info.serial_number}"
+    assert device.model_id == any_inverter.info.model
+    assert device.model == any_inverter.info.machine
+    assert device.sw_version == any_inverter.info.version
 
     # check entity registration
     entity_registry = er.async_get(hass)
-    entries = er.async_entries_for_config_entry(entity_registry, config_entry.entry_id)
+    entries = er.async_entries_for_config_entry(entity_registry, mock_config_entry.entry_id)
 
     platform_counts = Counter(entry.domain for entry in entries)
-    assert platform_counts[Platform.NUMBER] == 3
+    assert platform_counts[Platform.NUMBER] == 5
     assert platform_counts[Platform.SELECT] == 1
     assert platform_counts[Platform.SENSOR] == 5
     assert platform_counts[Platform.SWITCH] == 2
